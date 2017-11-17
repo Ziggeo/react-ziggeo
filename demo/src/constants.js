3c0d86b3e5e367a85c14c81e051a7ee652bd67df
Rashad Aliyev <aliyev.resad@gmail.com>
2017-11-18 00:03:17 +0400

    removed api keys after reset in system, and add some minor changes to doc

--- demo/src/constants.js
+++ demo/src/constants.js
@@ -1,12 +1,12 @@
 // TEST APPLICATION - 2  Test Application
-export const API_KEY = 'b2009d5f9513cc8f237fe4bd9d6f2ba';
-export const VIDEO_TOKEN = '507dcc5fe040cef6b2018e9ba45c5beb';
+export const API_KEY = '';
+export const VIDEO_TOKEN = '';
 
 // APP 2 - Default
-export const API_KEY_2 = '4ca2def526930999b0aae668913a09f7';
-export const VIDEO_TOKEN_2 = 'fbea9e262ab9371fbd70c73e1fa211f9';
-export const VIDEO_TOKEN_2_1 = '9c79e69e1279525c9107be7f76f9c3d5';
-export const VIDEO_TOKEN_AUDIO = '32c507b0de52bd0044bb72d9f41a06c6';
+export const API_KEY_2 = '';
+export const VIDEO_TOKEN_2 = '';
+export const VIDEO_TOKEN_2_1 = '';
+export const VIDEO_TOKEN_AUDIO = ''
 
 export const videos = [
     { video: VIDEO_TOKEN_2 },
