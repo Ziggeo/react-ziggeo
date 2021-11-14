1417f290c433fc5662c9020ebe7f562cb4844d3d
Rashad Aliyev <aliyev.resad@gmail.com>
2021-11-14 12:58:52 +0400

    Added player components

--- src/constants.js
+++ src/constants.js
@@ -243,6 +243,161 @@ export const ziggeoPlayerAttributesPropTypes = {
     // },
 };
 
+export const ziggeoAudioRecorderAttributesPropTypes = {
+    // Presentational parameters
+    'width': oneOfType([number, string]),
+    'height': oneOfType([number, string]),
+    'responsive': bool,
+    'skipinitial': bool,
+    'skipinitialonrerecord': bool,
+    'countdown': number,
+    'nofullscreen': bool,
+    'localplayback': bool,
+    'theme': string,
+    'themecolor': string,
+    'autorecord': bool,
+    'primaryrecord': bool,
+    'early-rerecord': bool,
+    'transcript-language': string,
+    'allowtexttrackupload': bool,
+    'uploadlocales': array,
+    'audio-test-mandatory':bool,
+    'playermodeifexists': bool,
+    'rerecordableifexists': bool,
+    'popup-width': oneOfType([number, string]),
+    'popup-height': oneOfType([number, string]),
+
+    // Security parameters
+    'server-auth': string,
+    'client-auth': string,
+    'expiration-days': number,
+
+    // Video management parameters
+    'audio': string,
+    '_key': string,
+    "rtmpstreamtype": string,
+    "rtmpmicrophonecodec": string,
+    'timelimit': number,
+    'timeminlimit': number,
+    'audio-profile': oneOfType([string, arrayOf(string)]),
+    'effect-profile': oneOfType([string, arrayOf(string)]),
+    'meta-profile': oneOfType([string, arrayOf(string)]),
+    'enforce-duration': bool,
+    'source':	string,
+    'audiobitrate': oneOfType([number, string]),
+    'microphone-volume': number,
+    'pausable': bool,
+
+    // Data parameters
+    'tags': arrayOf(string),
+    'custom-data':	object,
+    'title':	string,
+    'description': string,
+
+    // Operational parameters
+    'manual_upload': bool,
+    'rerecordable':	bool,
+    'allowupload': bool,
+    'allowrecord':	bool,
+    'display-timer': bool,
+    'force-overwrite':	bool,
+    'autoplay': bool,
+    'recordings': number,
+    'allowedextensions': arrayOf(string),
+    'application': string, //	oneOfType([object, string]),
+    'filesizelimit': number,
+    'simulate': bool,
+    'allowcustomupload': bool,
+    'manual-upload': bool,
+    'skipseconds': number,
+    'showsettingsmenu': bool, // As a property show/hide from users
+    'hidevolumebar': bool,
+    'recordermode': bool,
+    'manualsubmit': bool,
+    'initialseek': number,
+    'playwhenvisible': bool,
+    'playonclick': bool,
+    'disableseeking': bool,
+    'disablepause': bool,
+    'pauseonplay': bool,
+    'airplay': bool,
+    'chromecast': bool,
+    'preload': bool,
+    'delete-old-streams': bool,
+    'lazy-application': bool,
+    'allowcancel': bool,
+    'popup': bool,
+
+    // Form and HTML parameters
+    'input-bind': string,
+    'form-accept': string
+};
+
+export const ziggeoAudioPlayerAttributesPropTypes = {
+    // Presentational parameters
+    'width': oneOfType([number, string]),
+    'height': oneOfType([number, string]),
+    'skipinitial': bool,
+    'countdown': number,
+    'localplayback': bool,
+    'theme': string,
+    'themecolor': string,
+    'loop': bool,
+    'loopall': bool,
+    'volume': number,
+    'showduration': bool,
+    'uploadlocales': array,
+    'allowtexttrackupload': bool,
+    'audio-transcription-as-subtitles': bool,
+    'playlist': oneOfType([array, string]),
+    'popup': bool,
+    'popup-width': oneOfType([number, string]),
+    'popup-height': oneOfType([number, string]),
+
+    // Security parameters
+    'server-auth': string,
+    'client-auth': string,
+    'hideoninactivity': bool,
+    'hidebarafter': number, // in milliseconds
+    'preventinteraction': bool,
+
+    // Audio management parameters
+    'audio': string.isRequired,
+    '_key': string,
+    'effect-profile': oneOfType([string, arrayOf(string)]),
+    'source': string,
+    'audiobitrate': oneOfType([number, string]),
+
+    // Data parameters
+    'tags': arrayOf(string),
+    'custom-data':	object,
+    'title':	string,
+    'description': string,
+
+    // Operational parameters
+    'autoplay': bool,
+    'application': string, // oneOfType([object, string]),
+    'flashincognitosupport': bool,
+    'forceflash': bool,
+    'noflash': bool,
+    'initialseek': number,
+    'playwhenvisible': bool,
+    'playonclick': bool,
+    'disableseeking': bool,
+    'disablepause': bool,
+    'pauseonplay': bool,
+    'airplay': bool,
+    'chromecast': bool,
+    'preload': bool,
+    'lazy-application': bool,
+    'preroll': bool,
+    'vast': arrayOf(object),
+
+    // Form and HTML parameters
+    'input-bind': string,
+    'form-accept': string
+};
+
 // #######################  DEFAULTS  ##################################
 
 
